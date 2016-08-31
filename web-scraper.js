f47638201b085057f5ef418ded2159b63ce035a8
ChrisVbot <chris.veillette@gmail.com>
2016-08-31 01:40:53 +0000

    added column headers using csv-writer npm

--- web-scraper.js
+++ web-scraper.js
@@ -3,29 +3,32 @@ const url = process.argv[2];
 const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
+const csvWriter = require('csv-write-stream');
+const writer = csvWriter({headers:['file permission', 'absolute url', 'file type']});
+
 
 var stream = fs.createWriteStream('images.csv');
-  stream.on('open', () => {
-    request(url, (error, response, body) => {
-      if(!error && response.statusCode == 200) {    
-        $ = cheerio.load(body);
-        $('tr').each(function(index, element){
-          var filePermission = ($(this).children().first().text());
-          var fileName = ($(this).children().last().text());
-          var fileType = fileName.substr(fileName.indexOf(".") + 1);
-          if (fileType.length !== 3){
-            fileType = "directory";
-          }
-          var absoluteURL = (url + fileName);
-          csvInput = (filePermission + "," + absoluteURL + "," + fileType) + "\n";
-        //still in the each loop - writes each result to the csv file in a stream
-        stream.write(csvInput)
-        });
-      };
-      stream.end();    
-    });
-})
-
+  writer.pipe(stream);
+    stream.on('open', () => {
+      request(url, (error, response, body) => {
+        if(!error && response.statusCode == 200) {    
+          $ = cheerio.load(body);
+          $('tr').each(function(index, element){
+            var filePermission = ($(this).children().first().text());
+            var fileName = ($(this).children().last().text());
+            var fileType = fileName.substr(fileName.indexOf(".") + 1);
+            if (fileType.length !== 3){
+              fileType = "directory";
+            }
+            var absoluteURL = (url + fileName);
+            csvInput = (filePermission + "," + absoluteURL + "," + fileType) + "\n";
+          //still in the each loop - writes each result to the csv file in a stream
+          writer.write([filePermission, absoluteURL, fileType])
+          });
+        };
+    stream.end();    
+  });
+});
 
 
 
