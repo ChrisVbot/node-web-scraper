a9687b516c99b49929d52bab705150665ab711ed
ChrisVbot <chris.veillette@gmail.com>
2016-08-31 02:04:11 +0000

    added module functionality

--- web-scraper.js
+++ web-scraper.js
@@ -7,28 +7,31 @@ const csvWriter = require('csv-write-stream');
 const writer = csvWriter({headers:['file permission', 'absolute url', 'file type']});
 
 
-var stream = fs.createWriteStream('images.csv');
-  writer.pipe(stream);
-  stream.on('open', () => {
-    request(url, (error, response, body) => {
-      if(!error && response.statusCode == 200) {    
-        $ = cheerio.load(body);
-        $('tr').each(function(index, element){
-          var filePermission = ($(this).children().first().text());
-          var fileName = ($(this).children().last().text());
-          var fileType = fileName.substr(fileName.indexOf(".") + 1);
-          if (fileType.length !== 3){
-            fileType = "directory";
-          }
-          var absoluteURL = (url + fileName);
-          //still in the each loop - writes each result to the csv file in a stream. Formatted by csvWriter npm.
-          writer.write([filePermission, absoluteURL, fileType])
-        });
-      };
-  stream.end();    
+function webScraper(url) {
+  var stream = fs.createWriteStream('images.csv');
+    writer.pipe(stream);
+    stream.on('open', () => {
+      request(url, (error, response, body) => {
+        if(!error && response.statusCode == 200) {    
+          $ = cheerio.load(body);
+          $('tr').each(function(index, element){
+            var filePermission = ($(this).children().first().text());
+            var fileName = ($(this).children().last().text());
+            var fileType = fileName.substr(fileName.indexOf(".") + 1);
+            if (fileType.length !== 3){
+              fileType = "directory";
+            }
+            var absoluteURL = (url + fileName);
+            //still in the each loop - writes each result to the csv file in a stream. Formatted by csvWriter npm.
+            writer.write([filePermission, absoluteURL, fileType])
+          });
+        };
+    stream.end();    
+    });
   });
-});
+};
 
+module.exports = webScraper;
 
 
 
