7c9a0dd4f8c45294c1d5725f8295d495a01e5fcb
ChrisVbot <chris.veillette@gmail.com>
2016-08-31 01:46:36 +0000

    minor formatting fixes

--- web-scraper.js
+++ web-scraper.js
@@ -9,24 +9,24 @@ const writer = csvWriter({headers:['file permission', 'absolute url', 'file type
 
 var stream = fs.createWriteStream('images.csv');
   writer.pipe(stream);
-    stream.on('open', () => {
-      request(url, (error, response, body) => {
-        if(!error && response.statusCode == 200) {    
-          $ = cheerio.load(body);
-          $('tr').each(function(index, element){
-            var filePermission = ($(this).children().first().text());
-            var fileName = ($(this).children().last().text());
-            var fileType = fileName.substr(fileName.indexOf(".") + 1);
-            if (fileType.length !== 3){
-              fileType = "directory";
-            }
-            var absoluteURL = (url + fileName);
-            csvInput = (filePermission + "," + absoluteURL + "," + fileType) + "\n";
-          //still in the each loop - writes each result to the csv file in a stream
+  stream.on('open', () => {
+    request(url, (error, response, body) => {
+      if(!error && response.statusCode == 200) {    
+        $ = cheerio.load(body);
+        $('tr').each(function(index, element){
+          var filePermission = ($(this).children().first().text());
+          var fileName = ($(this).children().last().text());
+          var fileType = fileName.substr(fileName.indexOf(".") + 1);
+          if (fileType.length !== 3){
+            fileType = "directory";
+          }
+          var absoluteURL = (url + fileName);
+          csvInput = (filePermission + "," + absoluteURL + "," + fileType) + "\n";
+          //still in the each loop - writes each result to the csv file in a stream. Formatted by csvWriter npm.
           writer.write([filePermission, absoluteURL, fileType])
-          });
-        };
-    stream.end();    
+        });
+      };
+  stream.end();    
   });
 });
 
